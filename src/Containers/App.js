a7bc96be7b35c55d1da4de02098cb778214b6844
Tyler Kirby <joseph.kirby12@ncf.edu>
2017-08-22 12:26:31 -0500

    cleaned imports and unnecessary css files

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
-import _ from 'underscore'
 
 class App extends React.Component {
   render() {
